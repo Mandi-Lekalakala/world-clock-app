aa8a8a2e0a5cf45a4e57dbc3e1e569655071d1a2
Mandi-Lekalakala <mandilekalakala@gmail.com>
2026-06-05 15:47:01 +0200

    created user local time functionality. it is working in real time now

--- javascript/home.js
+++ javascript/home.js
@@ -1,14 +1,50 @@
+// ===USER'S LOCAL TIME===//
+
+function updateUserCity(cityName) {
+  let userCityName = document.getElementById("user-city");
+  let userTime = document.getElementById("user-time");
+  let userDate = document.getElementById("user-date");
+  let now = moment();
+  let abbr = moment.tz(moment.tz.guess()).format("z");
+
+  userCityName.innerHTML = `${cityName} · ${abbr}`;
+  userTime.innerHTML = now.format("HH:mm:ss");
+  userDate.innerHTML = now.format("dddd, D MMMM YYYY");
+
+  setInterval(function () {
+    updateUserCity(cityName);
+  }, 1000);
+}
+
+function handleGeolocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`,
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let cityName = data.city || data.locality;
+      updateUserCity(cityName);
+    });
+}
+
+navigator.geolocation.getCurrentPosition(handleGeolocation);
+
 //===FEATURED CITY TIMES===//
 
 function updateAllCards() {
   cityCards.forEach(function (card) {
     const zone = card.dataset.zone;
 
-    const timeDisplay = card.querySelector(".city-card-time");
+    const timeDisplay = card.querySelector("#city-card-time");
     const time = moment().tz(zone).format("HH:mm:ss");
     timeDisplay.innerHTML = time;
 
-    const offsetDisplay = card.querySelector("p");
+    const offsetDisplay = card.querySelector("#city-gmt");
     const offset = moment().tz(zone).format("Z");
     offsetDisplay.innerHTML = "GMT " + offset;
   });
